befdac6297aa43263f07dfdf8b5e0493fa64cdf6
Karl Butterworth <karlgbutterworth@gmail.com>
2024-11-04 21:56:31 +0000

    corrected port, duplicate was ignored as a change

--- main.tf
+++ main.tf
@@ -85,8 +85,8 @@ resource "aws_security_group" "allow_ping" {
   }
 
   ingress {
-    from_port   = 443
-    to_port     = 443
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["10.0.0.0/16"] # Internal CIDR for ping
   }
